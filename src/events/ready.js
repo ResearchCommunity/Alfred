5243c5eb5ec3b4a93bb3c09845f36967f532db9c
Xenorio <dev@xenorio.xyz>
2022-11-30 18:38:02 +0100

    add emergency player

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,10 +1,20 @@
 // Copyright (C) 2022  Xenorio
 // See license in /LICENSE
 
+const config = require('../../config')
+
 module.exports = async(client) => {
 
     console.log('Ready to rumble!')
 
-    require('../music/player').play()
+    let player
+
+    if (config.music.emergency) {
+        player = '../music/emergency-player'
+    } else {
+        player = '../music/player'
+    }
+
+    require(player).play()
 
 }
