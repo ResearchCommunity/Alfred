92b8c6f51f4f2172c289cfa81fbc0bb19384f8eb
Xenorio <dev@xenorio.xyz>
2022-12-06 02:50:09 +0100

    fix incorrect client attachment to API

--- src/api/server.js
+++ src/api/server.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 
 const app = express()
 
-let client
+let discord
 
 let paths = {}
 
@@ -22,19 +22,18 @@ app.use(express.urlencoded({extended: true}))
 app.use((req, res, next) => {
 
     let path = req.originalUrl.split('?')[0]
-    console.log(path)
     let route = paths[path]
 
     if (!route) return res.status(404).send('404 - Not Found')
 
     if (!route[req.method]) return res.status(404).send('404 - Not Found - Incorrect Request Method')
 
-    route[req.method](client, req, res, next)
+    route[req.method](discord, req, res, next)
 
 })
 
 module.exports.start = (client, port) => {
-    this.client = client
+    discord = client
     app.listen(port, err => {
         if (err) return console.error(err)
         console.log(`API Listening on port ${port}`)
