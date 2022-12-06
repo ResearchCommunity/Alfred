5d7a2d420e726c05ccfff3f933bc7e3907f05738
Xenorio <dev@xenorio.xyz>
2022-12-06 03:28:21 +0100

    cache shenanigans

--- src/components/rules-accept.js
+++ src/components/rules-accept.js
@@ -8,23 +8,26 @@ module.exports.execute = async(client, interaction) => {
 
     let url = config.onboarding.captcha.baseURL + `?user=${interaction.member.id}`
 
-    if (interaction.member.roles.has(config.onboarding.verifiedRole)) return interaction.reply({
-        ephemeral: true,
-        content: 'You have already accepted the rules!'
-    })
+    interaction.member.fetch(true)
+        .then(member => {
+            if (member.roles.cache.has(config.onboarding.verifiedRole)) return interaction.reply({
+                ephemeral: true,
+                content: 'You have already accepted the rules!'
+            })
 
-    interaction.reply({
-        ephemeral: true,
-        content: 'Just one more step! We need to verify your humanity!',
-        components: [{
-            type: ComponentType.ActionRow,
-            components: [{
-                type: ComponentType.Button,
-                style: ButtonStyle.Link,
-                url: url,
-                label: 'Solve Captcha'
-            }]
-        }]
-    })
+            interaction.reply({
+                ephemeral: true,
+                content: 'Just one more step! We need to verify your humanity!',
+                components: [{
+                    type: ComponentType.ActionRow,
+                    components: [{
+                        type: ComponentType.Button,
+                        style: ButtonStyle.Link,
+                        url: url,
+                        label: 'Solve Captcha'
+                    }]
+                }]
+            })
+        })
 
 }
